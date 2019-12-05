97f9653cfcaa65a71a99a806d06adf13947c7d22
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2019-12-05 09:53:32 -0600

    Ball bounces off of the edges of the screen

--- SpriteKitProject/GameScene.swift
+++ SpriteKitProject/GameScene.swift
@@ -15,14 +15,72 @@ struct PhysicsCategory {
     static let brick: UInt32 = 0b10
 }
 
+func +(left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func -(left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func *(point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+func sqrt(a: CGFloat) -> CGFloat {
+    return CGFloat(sqrtf(Float(a)))
+}
+#endif
+
+extension CGPoint {
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+}
+
 class GameScene: SKScene {
     let ball = SKSpriteNode(imageNamed: "Ball")
     
     override func didMove(to view: SKView) {
-        backgroundColor = SKColor.black
-        ball.position = CGPoint(x: size.width / 2, y: size.height * 0.9)
-        addChild(ball)
-    }
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody?.categoryBitMask = PhysicsCategory.paddle
+
     
+        backgroundColor = SKColor.black
+        ball.position = CGPoint(x: size.width / 2, y: size.height * 0.1)
+        ball.size.width /= 32
+        ball.size.height /= 32
+        addChild(ball)
+        
+        ball.physicsBody = SKPhysicsBody(rectangleOf: ball.size)
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.categoryBitMask = PhysicsCategory.ball
+        ball.physicsBody?.contactTestBitMask = PhysicsCategory.none
+        ball.physicsBody?.collisionBitMask = PhysicsCategory.paddle
+        
+        ball.physicsBody?.restitution = 1.0;
+        ball.physicsBody?.friction = 0.0;
+        ball.physicsBody?.linearDamping = 0.0;
+        ball.physicsBody?.angularDamping = 0.0;
+        ball.physicsBody?.affectedByGravity = false;
+
+        let impulse = CGVector(dx: 3.0, dy: 3.0);
+        ball.physicsBody?.applyImpulse(impulse)
+    }
+}
+
+extension GameScene: SKPhysicsContactDelegate {
     
 }
+
